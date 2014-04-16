003ff9d151df2e4e69df58b5db85a9c7ca5f72af
Steve Dekorte <steve@dekorte.com>
2014-04-16 15:11:58 -0700

    moving this stuff in from Bitmessage client project

---
+++ FoundationCategoriesKit/FoundationCategoriesKit.h
@@ -0,0 +1,17 @@
+//
+//  FoundationCategoriesKit.h
+//  FoundationCategoriesKit
+//
+//  Created by Steve Dekorte on 4/16/14.
+//  Copyright (c) 2014 voluntary.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#import "NSArray+extra.h"
+#import "NSDictionary+json.h"
+#import "NSFileManager+DirectoryLocations.h"
+#import "NSMutableAttributedString+extra.h"
+#import "NSObject+extra.h"
+#import "NSString+BM.h"
